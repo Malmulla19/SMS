ba83f55a6a67873dc2cae6b2b4d39d65ecc68039
[Mahdi] <[20180401@stu.uob.edu.bh]>
2022-10-18 10:23:18 +0300

    SMS

---
+++ sms.py
@@ -0,0 +1,22 @@
+import vonage
+from dotenv import load_dotenv
+import os
+load_dotenv()
+APIKEY = os.getenv("key")
+APISECRET = os.getenv("secret")
+print(APIKEY, APISECRET)
+client = vonage.Client(key=APIKEY, secret=APISECRET)
+sms = vonage.Sms(client)
+responseData = sms.send_message(
+    {
+        "from": "BIBF",
+        "to": "97339003955",
+        "text": "DO NOT FORGET THE EVALUATION",
+    }
+)
+
+if responseData["messages"][0]["status"] == "0":
+    print("Message sent successfully.")
+else:
+    print(
+        f"Message failed with error: {responseData['messages'][0]['error-text']}")
