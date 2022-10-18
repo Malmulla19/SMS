1158352197bd614f8c9a4ed432028077153bf688
[Mahdi] <[20180401@stu.uob.edu.bh]>
2022-10-18 13:50:10 +0300

    Commit

--- sms.py
+++ sms.py
@@ -9,14 +9,16 @@ client = vonage.Client(key=APIKEY, secret=APISECRET)
 sms = vonage.Sms(client)
 responseData = sms.send_message(
     {
-        "from": "N.B.B",
-        "to": "97339003955",
+        "from": "Any Name",
+        "to": "AnyNumber",
         "text": "DO NOT FORGET THE EVALUATION",
     }
 )
-
-if responseData["messages"][0]["status"] == "0":
-    print("Message sent successfully.")
-else:
-    print(
-        f"Message failed with error: {responseData['messages'][0]['error-text']}")
+try:
+    response = responseData["messages"][0]
+    if response["status"] == "0":
+        print("Message sent successfully.")
+    else:
+        print(f"Message failed with error: {response['error-text']}")
+except Exception as e:
+    print(f"Message failed with error: {e}")
